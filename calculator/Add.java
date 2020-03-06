2a038c6d06b6e514e1b84eea35b954bd55d2f25d
17pa1a0552 <47419120+17pa1a0552@users.noreply.github.com>
2020-03-06 18:53:46 +0530

    Add files via upload

---
+++ calculator/Add.java
@@ -0,0 +1,11 @@
+com.harisha.maven_demo
+class Add {
+    double x,y;
+    Add(double x,double y){
+        this.x=x;
+        this.y=y;
+    }
+    void add(){
+        System.out.println(this.x+this.y);
+    }
+}
